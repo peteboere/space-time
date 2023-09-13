ab230824bcc9344bc8d2d4e0e7f62ef4f02dd65f
Pete Boere <pete@the-echoplex.net>
2023-09-13 16:08:46 +0100

    Add JSDoc docs and type annotations
    * Relocate src into individual files
    * Remove license doc

---
+++ src/space.js
@@ -0,0 +1,82 @@
+const kilobyte = 1024;
+const megabyte = Math.pow(kilobyte, 2);
+const gigabyte = Math.pow(kilobyte, 3);
+const terabyte = Math.pow(kilobyte, 4);
+
+/**
+ * @param {string | number} size - Size expression or bytes integer
+ * @returns {number} Size in bytes
+ * @example
+ * bytes('1mb');
+ * bytes('12.5Mb');
+ * bytes('1 gigabyte + 12MB');
+ */
+export function bytes(size) {
+
+    if (typeof size !== 'string') {
+        return (typeof size === 'number')
+            ? size
+            : 0;
+    }
+
+    const patt = /(\d*\.?\d+)\s*(t|g|m|k|b)?/ig;
+
+    let bytes = 0;
+    let match;
+
+    while ((match = patt.exec(size))) {
+        const factor = parseFloat(match[1]);
+        const unit = match[2]?.toLowerCase();
+        switch (unit) {
+            case 't':
+                bytes += factor * terabyte;
+                break;
+            case 'g':
+                bytes += factor * gigabyte;
+                break;
+            case 'm':
+                bytes += factor * megabyte;
+                break;
+            case 'k':
+                bytes += factor * kilobyte;
+                break;
+            default:
+                bytes += factor;
+                break;
+        }
+    }
+
+    return bytes;
+}
+
+/**
+ * @param {string | number} size - Size expression or bytes integer
+ * @returns {number} Size in kilobytes
+ */
+export function kilobytes(size) {
+    return (bytes(size) / kilobyte) || 0;
+}
+
+/**
+ * @param {string | number} size - Size expression or bytes integer
+ * @returns {number} Size in megabytes
+ */
+export function megabytes(size) {
+    return (bytes(size) / megabyte) || 0;
+}
+
+/**
+ * @param {string | number} size - Size expression or bytes integer
+ * @returns {number} Size in gigabytes
+ */
+export function gigabytes(size) {
+    return (bytes(size) / gigabyte) || 0;
+}
+
+/**
+ * @param {string | number} size - Size expression or bytes integer
+ * @returns {number} Size in terabytes
+ */
+export function terabytes(size) {
+    return (bytes(size) / terabyte) || 0;
+}
---
+++ src/time.js
@@ -0,0 +1,170 @@
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const week = day * 7;
+const lunarMonth = day * 29.53059;
+const year = day * 365.25;
+const month = (year / 12);
+
+/**
+ * @param {string | number} time - Time expression or milliseconds
+ * @returns {number} Milliseconds
+ * @example
+ * ms('1h');
+ * ms('1 hr and 30 mins');
+ * ms('52w 5h 1d');
+ */
+export function ms(time) {
+
+    if (typeof time !== 'string') {
+        return (typeof time === 'number')
+            ? time
+            : 0;
+    }
+
+    const patt = /(\d*\.?\d+)\s*(y|m(?:[sot])|l|w|d|h|m|s)?/ig;
+
+    let ms = 0;
+    let match;
+
+    while ((match = patt.exec(time))) {
+        const factor = parseFloat(match[1]);
+        const unit = match[2]?.toLowerCase();
+        switch (unit) {
+            case 'y':
+                ms += factor * year;
+                break;
+            case 'l':
+                ms += factor * lunarMonth;
+                break;
+            case 'ms':
+                ms += factor;
+                break;
+            case 'mo': // fallthrough
+            case 'mt':
+                ms += factor * month;
+                break;
+            case 'm':
+                ms += factor * minute;
+                break;
+            case 'w':
+                ms += factor * week;
+                break;
+            case 'd':
+                ms += factor * day;
+                break;
+            case 'h':
+                ms += factor * hour;
+                break;
+            case 's':
+                ms += factor * second;
+                break;
+            default:
+                ms += factor;
+                break;
+        }
+    }
+
+    return ms;
+}
+
+/** @memberof ms */
+ms.now = relativeTimeHandler(ms);
+
+/**
+ * @param {string | number} time - Time expression or milliseconds
+ * @returns {number} Seconds
+ */
+export function seconds(time) {
+    return (ms(time) / 1000) || 0;
+}
+
+/** @memberof seconds */
+seconds.now = relativeTimeHandler(seconds);
+
+/**
+ * @param {string | number} time - Time expression or milliseconds
+ * @returns {number} Minutes
+ */
+export function minutes(time) {
+    return (seconds(time) / 60) || 0;
+}
+
+/** @memberof minutes */
+minutes.now = relativeTimeHandler(minutes);
+
+/**
+ * @param {string | number} time - Time expression or milliseconds
+ * @returns {number} Hours
+ */
+export function hours(time) {
+    return (minutes(time) / 60) || 0;
+}
+
+/** @memberof hours */
+hours.now = relativeTimeHandler(hours);
+
+/**
+ * @param {string | number} time - Time expression or milliseconds
+ * @returns {number} Days
+ */
+export function days(time) {
+    return (hours(time) / 24) || 0;
+}
+
+/** @memberof days */
+days.now = relativeTimeHandler(days);
+
+/**
+ * @param {string | number} time - Time expression or milliseconds
+ * @returns {number} Weeks
+ */
+export function weeks(time) {
+    return (days(time) / 7) || 0;
+}
+
+/** @memberof weeks */
+weeks.now = relativeTimeHandler(weeks);
+
+/**
+ * @param {string | number} time - Time expression or milliseconds
+ * @returns {number} Months
+ */
+export function months(time) {
+    return (days(time) / (365.25 / 12)) || 0;
+}
+
+/** @memberof months */
+months.now = relativeTimeHandler(months);
+
+/**
+ * @param {string | number} time - Time expression or milliseconds
+ * @returns {number} Years
+ */
+export function years(time) {
+    return (months(time) / 12) || 0;
+}
+
+/** @memberof years */
+years.now = relativeTimeHandler(years);
+
+/**
+ * @returns {(
+ *    relativeTimeExpression: string,
+ *    options?: {now?: () => number},
+ * ) => number}
+ */
+function relativeTimeHandler(fn) {
+    return function now(arg, {now=Date.now}={}) {
+        const directionPatt = /^\s*([+-])\s*/;
+        const m = directionPatt.exec(arg);
+        if (! m) {
+            throw new TypeError(`Relative time argument requires a '+' or '-' prefix`);
+        }
+        arg = arg.replace(directionPatt, '');
+        return fn((m[1] === '-')
+            ? (now() - ms(arg))
+            : (now() + ms(arg)));
+    };
+}
